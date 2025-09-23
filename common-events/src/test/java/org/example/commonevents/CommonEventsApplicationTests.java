1adc97fc39ab43aa250960f5304fc2e293e382ff
th3_10rd <n.khaydaraliyev@gmail.com>
2025-09-23 16:11:28 +0500

    created a new module for Kafka events

---
+++ common-events/src/test/java/org/example/commonevents/CommonEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.commonevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonEventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
