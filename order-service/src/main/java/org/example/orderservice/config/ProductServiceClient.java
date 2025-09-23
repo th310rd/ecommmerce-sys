205dddbc8167758e1dce200b807771259d975bb5
th3_10rd <n.khaydaraliyev@gmail.com>
2025-09-23 16:08:19 +0500

    added Url to Feign client

--- order-service/src/main/java/org/example/orderservice/config/ProductServiceClient.java
+++ order-service/src/main/java/org/example/orderservice/config/ProductServiceClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "product-service")
+@FeignClient(name = "product-service",url = "http://localhost:8081")
 public interface ProductServiceClient {
     @GetMapping("/products/{id}")
     Product getProduct(@PathVariable Long id);
