3ad883b852542ead92dfa81c21121e2e3f4e7eec
th3_10rd <n.khaydaraliyev@gmail.com>
2025-09-30 16:25:48 +0500

    fixed problem with feign

--- order-service/src/main/java/org/example/orderservice/config/SecurityConfig.java
+++ order-service/src/main/java/org/example/orderservice/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.example.orderservice.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -23,6 +24,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.GET, "/orders/").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
